3a17824b33a4e42c71fd38dd9afbc3760b2739e1
Greg Thomson <GregisGinger2@googlemail.com>
2015-02-02 21:01:51 +0000

    Chapter 9 - completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rango.models import Category, Page
-from rango.forms import CategoryForm
-from rango.forms import PageForm
-from django.http import HttpResponse
+from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -10,9 +12,13 @@ def index(request):
     context_dict = {'categories': category_list, 'pages': page_list }
     return render(request, 'rango/index.html', context_dict)
 
+
+
 def about(request):
     context_dict = {'boldmessage': "I am bold font from the context"}
     return render(request, 'rango/about.html', context_dict)
+
+
 
 def category(request, category_name_slug):
     context_dict = {}
@@ -29,6 +35,17 @@ def category(request, category_name_slug):
 
     return render(request, 'rango/category.html', context_dict)
 
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/rango/')
+
+@login_required
 def add_category(request):
 
     if request.method == 'POST':
@@ -44,6 +61,8 @@ def add_category(request):
 
     return render(request, 'rango/add_category.html', {'form': form})
 
+
+@login_required
 def add_page(request, category_name_slug):
 
     try:
@@ -69,3 +88,67 @@ def add_page(request, category_name_slug):
     context_dict = {'form':form, 'category': cat, 'cat_slug': category_name_slug}
 
     return render(request, 'rango/add_page.html', context_dict)
+
+
+
+def register(request):
+
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+            registered = True
+
+        else:
+            print user_form.errors, profile_form.errors
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request,
+            'rango/register.html',
+            {'user_form': user_form, 'profile_form': profile_form, 'registered': registered} )
+
+
+
+def user_login(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/rango/')
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print "Invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse("<html>"
+                                + "<body>"
+                                + "<p>Invalid login details supplied. Username must be "
+                                + "30 characters or fewer. Letters, digits and @/./+/-/_ o"
+                                + "nly. </p><a href = '/rango/login/'>Go back</a>"
+                                + "</body>"
+                                +  "</html>")
+
+    else:
+
+        return render(request, 'rango/login.html', {})
